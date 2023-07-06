1a1fe4dbe796238b181a1e90dbe20cdc59886552
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-07-05 19:59:50 -0500

    feat: :sparkles: Creacion Endpoints historiales TB

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import storageGbp from './routers/bodegas.js';
 import storageGbpUsers from './routers/users.js'
 import storageGbpProductos from './routers/productos.js';
 import storageGbpInventarios from './routers/inventarios.js';
+import storageGbpHistoriales from './routers/historiales.js';
 dotenv.config();
 const appExpress = express();
 
@@ -12,6 +13,8 @@ appExpress.use("/gbp", storageGbp);
 appExpress.use("/gbp", storageGbpUsers);
 appExpress.use("/gbp", storageGbpProductos);
 appExpress.use("/gbp", storageGbpInventarios);
+appExpress.use("/gbp", storageGbpHistoriales);
+
 
 const config =JSON.parse(process.env.MY_CONFIG);
 appExpress.listen(config, ()=>console.log(`http://${config.hostname}:${config.port}`));
---
+++ routers/historiales.js
@@ -0,0 +1,90 @@
+import mysql from 'mysql2';
+import {Router} from 'express';
+const storageGbpHistoriales = Router();
+let con = undefined;
+
+storageGbpHistoriales.use((req, res, next) => {
+
+    let myConfig = JSON.parse(process.env.MY_CONNECT);
+    con = mysql.createPool(myConfig)
+    next();
+})
+
+storageGbpHistoriales.get("/historiales", (req, res) => {
+    con.query(
+        /*sql*/
+        `SELECT * FROM historiales`,
+        (err, data, fil) => {
+            res.send(JSON.stringify(data));
+        }
+    );
+})
+storageGbpHistoriales.get("/historiales/:id", (req, res) => {
+    const id = req.params.id;
+
+    con.query(
+        /*sql*/
+        `SELECT * FROM historiales WHERE id = ?`, [id],
+        (err, data, fil) => {
+            if (err) {
+                console.error('Error al obtener el historial:', err.message);
+                res.sendStatus(500);
+            } else {
+                res.send(JSON.stringify(data));
+            }
+        }
+    );
+});
+
+storageGbpHistoriales.post("/historiales", (req, res) => {
+    const {id,cantidad,id_bodega_origen,id_bodega_destino,id_inventario,estado,created_by,update_by,created_at,updated_at,deleted_at} = req.body;
+    con.query(
+        /*sql*/
+        `INSERT INTO historiales (id,cantidad,id_bodega_origen,id_bodega_destino,id_inventario,estado,created_by,update_by,created_at,updated_at,deleted_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+        [id,cantidad,id_bodega_origen,id_bodega_destino,id_inventario,estado,created_by,update_by,created_at,updated_at,deleted_at],
+        (err, result) => {
+            if (err) {
+                console.error('Error al crear el historial:', err.message);
+                res.sendStatus(500);
+            } else {
+                res.sendStatus(201);
+            }
+        }
+    );
+});
+storageGbpHistoriales.put("/historiales/:id", (req, res) => {
+    const id = req.params.id;
+    const {cantidad,id_bodega_origen,id_bodega_destino,id_inventario,estado,created_by,update_by,created_at,updated_at,deleted_at} = req.body;
+    con.query(
+        /*sql*/
+        `UPDATE historiales SET cantidad = ?,id_bodega_origen = ?,id_bodega_destino = ?,id_inventario = ?,estado = ?,created_by = ?,update_by = ?,created_at = ?,updated_at = ?,deleted_at = ? WHERE id = ?`,
+        [cantidad,id_bodega_origen,id_bodega_destino,id_inventario,estado,created_by,update_by,created_at,updated_at,deleted_at,id],
+        (err, result) => {
+            if (err) {
+                console.error('Error al actualizar el historial:', err.message);
+                res.sendStatus(500);
+            } else {
+                res.sendStatus(200);
+            }
+        }
+    );
+});
+storageGbpHistoriales.delete("/historiales/:id", (req, res) => {
+    const id = req.params.id;
+    con.query(
+        /*sql*/
+        `DELETE FROM historiales WHERE id = ?`,
+        [id],
+        (err, result) => {
+            if (err) {
+                console.error('Error al eliminar el historial:', err.message);
+                res.sendStatus(500);
+            } else {
+                res.sendStatus(200);
+            }
+        }
+    );
+});
+
+
+export default storageGbpHistoriales;
