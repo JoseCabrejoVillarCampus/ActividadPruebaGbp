e60930e8956b93e8b20661aa35115f605eef3d55
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-07-06 20:37:06 -0500

    feat: :sparkles: EndPolnt  Trasladar
    EndPolnt que permita Trasladar un producto de una bodega a otra
    
    falta arreglar los routing usando middleware

--- routers/productos.js
+++ routers/productos.js
@@ -89,6 +89,105 @@ storageGbpProductos.post("/productos", (req, res) => {
         }
     });
 });
+storageGbpProductos.post("/trasladar/:productoId", (req, res) => {
+    const productoId = req.params.productoId;
+    const origenId = req.body.origenId;
+    const destinoId = req.body.destinoId;
+    const cantidad = req.body.cantidad;
+  
+    con.getConnection((err, connection) => {
+      if (err) {
+        console.error('Error al obtener la conexión:', err.message);
+        res.sendStatus(500);
+      } else {
+        connection.beginTransaction((err) => {
+          if (err) {
+            console.error('Error al iniciar la transacción:', err.message);
+            res.sendStatus(500);
+            return;
+          }
+  
+          // Verificar la cantidad disponible en el origen
+          connection.query(
+            `SELECT cantidad FROM inventarios WHERE id_producto = ? AND id_bodega = ?`,
+            [productoId, origenId],
+            (err, result) => {
+              if (err) {
+                console.error('Error al obtener la cantidad en el origen:', err.message);
+                connection.rollback(() => {
+                  res.sendStatus(500);
+                });
+              } else {
+                const cantidadDisponible = result[0].cantidad;
+                if (cantidad > cantidadDisponible) {
+                  // La cantidad solicitada es mayor a la disponible en el origen
+                  res.status(400).json({ message: 'No hay suficiente cantidad disponible en el origen' });
+                  connection.rollback(() => {
+                    res.sendStatus(400);
+                  });
+                  return;
+                }
+  
+                // Actualizar la cantidad en el origen (restar)
+                connection.query(
+                  `UPDATE inventarios SET cantidad = cantidad - ? WHERE id_producto = ? AND id_bodega = ?`,
+                  [cantidad, productoId, origenId],
+                  (err, result) => {
+                    if (err) {
+                      console.error('Error al restar la cantidad del origen:', err.message);
+                      connection.rollback(() => {
+                        res.sendStatus(500);
+                      });
+                    } else {
+                      // Actualizar la cantidad en el destino (sumar)
+                      connection.query(
+                        `UPDATE inventarios SET cantidad = cantidad + ? WHERE id_producto = ? AND id_bodega = ?`,
+                        [cantidad, productoId, destinoId],
+                        (err, result) => {
+                          if (err) {
+                            console.error('Error al sumar la cantidad en el destino:', err.message);
+                            connection.rollback(() => {
+                              res.sendStatus(500);
+                            });
+                          } else {
+                            // Registrar el traslado en la tabla de historiales
+                            connection.query(
+                              `INSERT INTO historiales (id_producto, id_bodega_origen, id_bodega_destino, cantidad) 
+                              VALUES (?, ?, ?, ?)`,
+                              [productoId, origenId, destinoId, cantidad],
+                              (err, result) => {
+                                if (err) {
+                                  console.error('Error al registrar el traslado en el historial:', err.message);
+                                  connection.rollback(() => {
+                                    res.sendStatus(500);
+                                  });
+                                } else {
+                                  connection.commit((err) => {
+                                    if (err) {
+                                      console.error('Error al confirmar la transacción:', err.message);
+                                      connection.rollback(() => {
+                                        res.sendStatus(500);
+                                      });
+                                    } else {
+                                      res.sendStatus(201);
+                                    }
+                                  });
+                                }
+                              }
+                            );
+                          }
+                        }
+                      );
+                    }
+                  }
+                );
+              }
+            }
+          );
+        });
+      }
+    });
+  });
 storageGbpProductos.put("/:id", (req, res) => {
     const id = req.params.id;
     const {nombre,descripcion,estado,created_by,update_by,created_at,updated_at,deleted_at} = req.body;
